d26436bd358585422b9f6e684622f5540cf2257b
ha1601589@qu.edu.qa <ha1601589@qu.edu.qa>
2019-10-02 10:45:22 +0300

    shapes v1

---
+++ busConvert/routes.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sumolib
+
+shapes = np.genfromtxt('GTFS/shapes.txt', delimiter=",", dtype=[('shape_id', 'i8'), ('shape_pt_lat', 'f16'),
+                                                                ('shape_pt_lon', 'f16'), ('shape_pt_sequence', 'i8')])
+shapes = np.sort(shapes, order=['shape_id', 'shape_pt_sequence'])
+shape_ids = np.unique(shapes['shape_id'])
+net = sumolib.net.readNet('qatar.net.xml')
+
+
+def _get_edge_id(shape):
+    radius = 0.1
+    x, y = net.convertLonLat2XY(shape[2], shape[1])
+    edges = net.getNeighboringEdges(x, y, radius)
+    i = 0.1
+    while len(edges) == 0:
+        edges = net.getNeighboringEdges(x, y, radius + i)
+        i += 0.1
+    if len(edges) > 1:
+        distances_and_edges = sorted([(edge, dist) for edge, dist in edges], key=lambda e: e[1])
+        closest_edge, dist = distances_and_edges[0]
+        return closest_edge.getID()
+    else:
+        return edges[0][0].getID()
+
+
+def shape_to_edge_sequence(shape_id):
+    shape_sequence = shapes[shapes['shape_id'] == shape_id]
+    edges_sequence = []
+    for shape in shape_sequence:
+        edges_sequence.append(_get_edge_id(shape))
+    edges_sequence = np.array(edges_sequence)
+    edges_sequence = np.unique(edges_sequence)
+    return edges_sequence
